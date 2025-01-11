d0188e3661f69d78f1877af0a3fa00ceab6b0cf7
bcharles4 <95261817+bcharles4@users.noreply.github.com>
2025-01-12 03:27:43 +0800

    add uploads for the user

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,15 +1,18 @@
-import mongoose from "mongoose";
-
+// backend/models/users.model.js
 const usersSchema = new mongoose.Schema({ 
     usersID: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     password: { type: String, required: true},
-    profilePicture: { type: String}, // Add profile picture field
-},{
+    uploads: [{ // Added this field to store user uploads
+        title: String,
+        description: String,
+        filePath: String,
+        uploadedAt: { type: Date, default: Date.now }
+    }]
+}, {
     timestamps: true,
 });
-
 
 const Users = mongoose.model("Users", usersSchema);
 
